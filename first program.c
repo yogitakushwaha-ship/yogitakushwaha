a0c400dd1514ec5e80dd9a5ef929683a4ec40230
YOGITA KUSHWAHA <yogitakushwaha122@gmail.com>
2026-01-25 11:58:46 +0530

    Add files via upload

---
+++ first program.c
@@ -0,0 +1,18 @@
+#include<stdio.h.>
+void main()
+{
+    float length,width, area,perimeter.
+    // Input length and width
+    printf("enter the length of the rectangle:");
+    scanf("%f",$ length);
+
+    printf("enter the width of the rectangle:");
+    scanf("%f",$ width);
+
+    // calculate area and perimeter
+    area= length*width;
+    perimeter = 2*(length+width);
+}
+{
+
+}
