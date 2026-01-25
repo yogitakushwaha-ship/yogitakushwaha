a40c61633e2cc912e4c6f94f32c6d38b2f888902
YOGITA KUSHWAHA <yogitakushwaha122@gmail.com>
2026-01-25 11:53:55 +0530

    Add files via upload

---
+++ program to check whether a number is prime or not .c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int num,i;
+    printf("enter number");
+    scanf("%d",&num);
+    if(num<= 1)
+    {
+      printf("%d is not a prime number\n",num);
+    }
+     for(i=2;i<num;i++);
+     {
+         if(num%i==0);
+     {
+       is prime=0;
+       break;
+     }
+     }
+     print("%d is a prime number\n",num);
+     else
+     printf("%d is not a prime number\n
+
+
+}
