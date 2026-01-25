ae17715b89a06377f3f1b6b9770ed8f03e55ceae
YOGITA KUSHWAHA <yogitakushwaha122@gmail.com>
2026-01-25 11:55:01 +0530

    Add files via upload

---
+++ program to convert temperature from celsius to fahrenheit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float C,F;
+    printf("Enter the temperature in celsius");
+    scanf("%f",&C);
+    F=(C*9/5)+32;
+    printf("Fahrenheit temperature =%f",F);
+
+
+
+
+}
