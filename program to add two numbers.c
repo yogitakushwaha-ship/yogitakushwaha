1ed7344975484f24a6d229c1ce0bcde45ae840c0
YOGITA KUSHWAHA <yogitakushwaha122@gmail.com>
2026-01-25 11:52:45 +0530

    Add files via upload

---
+++ program to add two numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int var1,var2,sum;
+    printf("Enter two intger");
+    scanf("%d%d",&var1,&var2,&sum);
+    sum:var1+var2;
+    printf("The Sum of %d%d is%d\n",var1,var2,sum);
+
+}
