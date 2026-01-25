bd2146c64a1d2dab6be01c46c1d17a951719d4b3
YOGITA KUSHWAHA <yogitakushwaha122@gmail.com>
2026-01-25 12:05:10 +0530

    Add files via upload

---
+++ write a program to print first n natural enter by user.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int counter,n;
+    printf("enter value of n/t");
+    scanf("%d",&n);
+          for(counter=1;counter<=5;counter++)
+             printf("%d\n",counter);
+
+
+
+}
